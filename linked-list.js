b868d508e967de8aefe269518cd7e54300b25faf
Youssef-001 <youssefsamir432@gmail.com>
2024-08-14 18:50:15 +0300

    Add append, prepend, pop functionality for linked-list

---
+++ linked-list.js
@@ -0,0 +1,77 @@
+class LinkedList {
+  constructor() {
+    this.head = new Node(null);
+    this.size = 0;
+    this.ref = new Node(null);
+  }
+
+  append(item) {
+    this.size += 1;
+    const newNode = new Node(item);
+    if (this.size <= 1) {
+      this.head = newNode;
+      this.ref = this.head;
+    } else {
+      this.head.next = newNode;
+      this.head = this.head.next;
+    }
+  }
+
+  prepend(item) {
+    this.size += 1;
+    const newNode = new Node(item);
+    if (this.size <= 1) {
+      this.head = newNode;
+      this.ref = this.head;
+    } else {
+      newNode.next = this.ref;
+      this.ref = newNode;
+    }
+  }
+
+  pop() {
+    let val;
+    if (this.size == 0) return null;
+
+    this.size -= 1;
+    if (this.size == 1) {
+      this.head = null;
+      this.ref = null;
+      return this.head.value;
+    } else {
+      let temp = new Node(0);
+      temp = this.ref;
+
+      while (temp.next != this.head) {
+        temp = temp.next;
+      }
+      val = this.head.value;
+      temp.next = null;
+      this.head = temp;
+    }
+    return val;
+  }
+
+  //   at() {}
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+let list = new LinkedList();
+
+list.append(1);
+list.append(2);
+list.append(4);
+list.append(5);
+// list.prepend(8);
+// list.prepend(10);
+
+console.log(list.pop());
+console.log(list.pop());
+
+console.log(list.ref);
